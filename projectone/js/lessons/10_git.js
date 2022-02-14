534f118b1285c953be50131631c83919f72848a9
Alex <voskobovich555@gmail.com>
2022-02-14 22:26:12 +0300

    second

--- projectone/js/lessons/10_git.js
+++ projectone/js/lessons/10_git.js
@@ -13,10 +13,23 @@
 // git config --global user.email voskobovich555@gmail.com - объявление email пользователя в проекте глобально
 // git status - проверка состояний
 // git add -A (добавить все файлы в commit)
+// git add *.css - добавить все стилевые файлы
+// git add local/home/index.php - добавить 1 файл
+// git commit -a -m "Message1" - описание коммита котрый делается (можно и без -a)
+// git checkout - сбросить изменения которые были сделаны до предыдущего коммита
+// git log - посмотреть  историю коммитов
+// git remote add origin https://github.com/AlexxVosko/projectjs.git - добавление удаленного репозитория (remote add) +
+// название репозитория (origin) + url репозитория
 
+// git push -u origin main - добавление данных в ветку репозитория
 
 // 3 состояния
 // 1 - Файлы созданы
 // 2 - git следит за определенными файлами (попадают в индекс)
 // 3 - git создал контрольную точку, к которой можно вернуться (commit)
 
+
+
+//Github
+//https://github.com/new - новый репозиторий для проекта
+//
