e6cda74213d62862371e81d7f49deb5ac46f2a2c
AlexVosko <av@artismedia.by>
2022-02-17 13:28:56 +0300

    Practic #1

--- projectone/js/script.js
+++ projectone/js/script.js
@@ -1,4 +1,21 @@
 //console.log(5);
-"use strict";
-let number = 5;
-const leftBorderWidth = 50;
+'use strict';
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+const personalMovieDB = {};
+personalMovieDB.count=numberOfFilms;
+personalMovieDB.movies={};
+personalMovieDB.actors={};
+personalMovieDB.genres=[];
+personalMovieDB.privat=false;
+
+const films = prompt("Один из последних просмотренных фильмов?",''),
+    mark = +prompt("На сколько оцените его?",''),
+    films1 = prompt("Один из последних просмотренных фильмов?",''),
+    mark1 = +prompt("На сколько оцените его?",'');
+
+personalMovieDB.movies[films] = mark;
+personalMovieDB.movies[films1] = mark1;
+
+ 
+console.log(personalMovieDB);
