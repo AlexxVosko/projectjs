979d6089c02aafdf76096731a7ad78771b96cbb7
AlexVosko <av@artismedia.by>
2022-02-22 23:16:04 +0300

    Practic 2

--- projectone/js/script.js
+++ projectone/js/script.js
@@ -9,13 +9,27 @@ personalMovieDB.actors={};
 personalMovieDB.genres=[];
 personalMovieDB.privat=false;
 
-const films = prompt("Один из последних просмотренных фильмов?",''),
-    mark = +prompt("На сколько оцените его?",''),
-    films1 = prompt("Один из последних просмотренных фильмов?",''),
-    mark1 = +prompt("На сколько оцените его?",'');
 
-personalMovieDB.movies[films] = mark;
-personalMovieDB.movies[films1] = mark1;
+let num2 =1;
+while(num2<3){
+    let films = prompt("Один из последних просмотренных фильмов?",''),
+    mark = +prompt("На сколько оцените его?",'');
 
+    if(mark!='' &&  mark!=null  && films!='' && films.length<50 && films!=null){   // Условие в виде не пустой строки, название не длинее, чем 50 символов и отменить ответ
+            personalMovieDB.movies[films]=mark;
+            num2++;
+    }
+} 
+
+if(personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count>=10 && personalMovieDB.count<30){
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count>=30){
+    console.log("Вы киноман");
+}else{
+    console.log("Произошла ошибка");
+}
  
 console.log(personalMovieDB);
+ 
