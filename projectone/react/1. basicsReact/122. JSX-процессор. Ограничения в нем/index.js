8fafa9bcc9b2083bf37a395792b546bf1e95b3ba
AlexxVosko <av@artismedia.by>
2022-05-26 17:15:15 +0300

    UPD 26.05.2022

---
+++ projectone/react/1. basicsReact/122. JSX-процессор. Ограничения в нем/index.js
@@ -0,0 +1,11 @@
+
+
+// первые строчки в index.js - импорты
+
+// import React from 'react'; - импорт библиотеки React (а точнее класс) отвечает за работу с  React
+// import ReactDOM from 'react-dom/client'; позволяет работать с DOM страницей (позволяет вставлять библиотеку React)
+// import './index.css'; - импорт стилей
+//import App from './App'; - импорт функции
+//import reportWebVitals from './reportWebVitals'; - импорт сторонней библиотеки (как быстро работает приложение)
+
+
